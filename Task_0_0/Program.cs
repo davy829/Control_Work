10a0bc6b082d11262ab5e1e08eb37b076b490bb0
Andrey DALI <daandrey485@gmail.com>
2023-04-06 22:57:47 +0300

    program work but need added function and optimization

--- Task_0_0/Program.cs
+++ Task_0_0/Program.cs
@@ -2,14 +2,35 @@
 
 
 Console.WriteLine("Напишите пять слов после каждого жмите Ентер_ ");
- string[] massiv = new string[6];
+string[] massiv = new string[6];
+int count = 0;
+int index=0;
+string myword=string.Empty;
 for (int i = 0; i < massiv.Length; i++)
 {
-    massiv[i]=Console.ReadLine()!;
+    massiv[i] = Console.ReadLine()!;
+
 }
-
-
+Console.WriteLine("_____________________________________________ ");
+Console.WriteLine("your short words what have max 3 simbols: ");
 for (int i = 0; i < massiv.Length; i++)
 {
-  Console.WriteLine($"{i}, {massiv[i]}");
+    myword= massiv[i];
+    if (myword.Length <= 3)
+    {
+        count++;
+    }
+}
+string[] shortmassiv = new string[count];
+for (int i = 0; i < massiv.Length; i++)
+{
+    if (massiv[i].Length <= 3)
+    {
+    shortmassiv[index] = massiv[i];
+    index++;
+    }
+}
+for (int i = 0; i < count; i++)
+{
+  Console.WriteLine($"{shortmassiv[i]}");  
 }
