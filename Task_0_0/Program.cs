ebab47d8bec9a80d9e3eaf999830ae565249dd15
Andrey DALI <daandrey485@gmail.com>
2023-04-06 22:06:38 +0300

    user input five words without programm function

--- Task_0_0/Program.cs
+++ Task_0_0/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Напишите пять слов после каждого жмите Ентер_ ");
+ string[] massiv = new string[6];
+for (int i = 0; i < massiv.Length; i++)
+{
+    massiv[i]=Console.ReadLine()!;
+}
+
+
+for (int i = 0; i < massiv.Length; i++)
+{
+  Console.WriteLine($"{i}, {massiv[i]}");
+}
